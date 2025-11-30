31553cde7ebb1f75e6556f6a80c8801a865458ff
ritwik-raj30 <btech10199.22@bitmesra.ac.in>
2025-11-30 22:52:15 +0530

    Remove broken submodule reference

--- Backend/locustfile.py
+++ Backend/locustfile.py
@@ -1,9 +1,9 @@
-from locust import HttpUser, task, between
+# from locust import HttpUser, task, between
 
-class BasicLoadTest(HttpUser):
-    wait_time = between(1, 2)
+# class BasicLoadTest(HttpUser):
+#     wait_time = between(1, 2)
 
-    @task
-    def health_check(self):
-        self.client.get("/")
+#     @task
+#     def health_check(self):
+#         self.client.get("/")
 
